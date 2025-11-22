ed15fbc255eef880b9255658cd7d8393a780dc3d
anaprates-r <ana.prates@ufms.br>
2025-11-22 14:59:06 -0400

    Add Usuario entity with fields and methods

---
+++ src/main/java/com/cine/cineme/model/Usuario.java
@@ -0,0 +1,55 @@
+package com.cine.cineme.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    private String id;
+    private String nome;
+    private String nomeUsuario;
+    private String senha;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeUsuario() {
+        return nomeUsuario;
+    }
+
+    public void setNomeUsuario(String nomeUsuario) {
+        this.nomeUsuario = nomeUsuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", nomeUsuario='" + nomeUsuario + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+}
