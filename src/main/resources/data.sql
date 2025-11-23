9a814e89b3809421880afe9cf967923c2ae45950
anaprates-r <ana.prates@ufms.br>
2025-11-23 09:29:29 -0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,19 @@ create table Conteudo (
     fotoUrl varchar(255) not null,
     tipo varchar(20) not null
 );
+create table Usuario(
+    id varchar(255) not null primary key,
+    nome varchar(100) not null,
+    nomeUsuario varchar(50) not null,
+    senha varchar(255) not null
+);
+create table Avaliacao(
+    id varchar(255) not null primary key,
+    nota int not null,
+    avaliacao varchar(255),
+    dataAvaliacao DATE default current_date,
+    usuarioId varchar(255),
+    conteudoId varchar(255),
+    foreign key (usuarioId) references Usuario(id),
+    foreign key (conteudoId) references Conteudo(id)
+);
