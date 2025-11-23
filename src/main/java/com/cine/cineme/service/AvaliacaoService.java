4d850cb2db90e1ce4c6977afaf013cbc66b110eb
anaprates-r <ana.prates@ufms.br>
2025-11-23 17:22:38 -0400

    Update AvaliacaoService.java

--- src/main/java/com/cine/cineme/service/AvaliacaoService.java
+++ src/main/java/com/cine/cineme/service/AvaliacaoService.java
@@ -1,8 +1,11 @@
 package com.cine.cineme.service;
 
 import com.cine.cineme.model.Avaliacao;
+import com.cine.cineme.model.Conteudo;
+import com.cine.cineme.model.Usuario;
 import com.cine.cineme.repository.AvaliacaoRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -14,11 +17,44 @@ public class AvaliacaoService {
         this.avaliacaoRepository = avaliacaoRepository;
     }
 
-    public List<Avaliacao> buscarAvaliacoesPorUsuario(String usuarioId) {
-        return avaliacaoRepository.findByUsuarioId(usuarioId);
+    public List<Avaliacao> listarPorConteudo(String conteudoId) {
+        return avaliacaoRepository.findByConteudoId(conteudoId);
     }
     
     public void excluirAvaliacao(String id) {
         avaliacaoRepository.deleteById(id);
+    }
+
+    public List<Avaliacao> buscarAvaliacoesPorUsuario(String usuarioId) {
+        return avaliacaoRepository.findByUsuarioId(usuarioId);
+    }
+
+    public Double calcularMedia(String conteudoId) {
+        Double media = avaliacaoRepository.calcularMedia(conteudoId);
+        return media != null ? media : 0.0;
+    }
+
+    public Avaliacao buscarAvaliacaoDoUsuario(String usuarioId, String conteudoId) {
+        return avaliacaoRepository.findByUsuarioIdAndConteudoId(usuarioId, conteudoId);
+    }
+
+    public Avaliacao salvar(Usuario usuario, Conteudo conteudo, int nota, String comentario) {
+
+        // Verifica se o usuário já avaliou antes
+        Avaliacao existente = avaliacaoRepository.findByUsuarioIdAndConteudoId(usuario.getId(), conteudo.getId());
+
+        if (existente != null) {
+            existente.setNota(nota);
+            existente.setComentario(comentario);
+            return avaliacaoRepository.save(existente); // atualiza
+        }
+
+        Avaliacao nova = new Avaliacao();
+        nova.setNota(nota);
+        nova.setComentario(comentario);
+        nova.setUsuario(usuario);
+        nova.setConteudo(conteudo);
+
+        return avaliacaoRepository.save(nova);
     }
 }
