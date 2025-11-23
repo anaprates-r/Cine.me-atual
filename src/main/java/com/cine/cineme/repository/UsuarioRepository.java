e06444de211efadde7c1353221b26c85aa028a7c
anaprates-r <ana.prates@ufms.br>
2025-11-23 17:16:42 -0400

    Add method to find Usuario by nomeUsuario

--- src/main/java/com/cine/cineme/repository/UsuarioRepository.java
+++ src/main/java/com/cine/cineme/repository/UsuarioRepository.java
@@ -4,4 +4,6 @@ import com.cine.cineme.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+    Usuario findByNomeUsuario(String nomeUsuario);
 }
