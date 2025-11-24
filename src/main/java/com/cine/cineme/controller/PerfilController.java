be3485143d95ad928f384194406d5e517978dc05
anaprates-r <ana.prates@ufms.br>
2025-11-24 16:39:09 -0400

    Refactor PerfilController to include UsuarioService

--- src/main/java/com/cine/cineme/controller/PerfilController.java
+++ src/main/java/com/cine/cineme/controller/PerfilController.java
@@ -1,32 +1,38 @@
-package com.cine.cineme.controller; 
+package com.cine.cineme.controller;
 
-import com.cine.cineme.model.Avaliacao; 
+import com.cine.cineme.model.Avaliacao;
+import com.cine.cineme.model.Usuario;
 import com.cine.cineme.service.AvaliacaoService;
+import com.cine.cineme.service.UsuarioService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable; 
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
-@Controller 
+@Controller
 public class PerfilController {
 
+    private final UsuarioService usuarioService;
     private final AvaliacaoService avaliacaoService;
-    private static final String ID_USUARIO_LOGADO = "1"; // <<-- ID DE TESTE
+    private static final String ID_USUARIO_LOGADO = "u1"; // <<-- ID DE TESTE
 
-    public PerfilController(AvaliacaoService avaliacaoService) {
+    public PerfilController(AvaliacaoService avaliacaoService, UsuarioService usuarioService) {
         this.avaliacaoService = avaliacaoService;
+        this.usuarioService = usuarioService;
     }
 
-    @GetMapping("/perfil") 
+    @GetMapping("/perfil")
     public String verPerfil(Model model) {
-        
+
         List<Avaliacao> avaliacoesReais = avaliacaoService.buscarAvaliacoesPorUsuario(ID_USUARIO_LOGADO);
 
+        Usuario usuario = usuarioService.buscarPorId(ID_USUARIO_LOGADO);
+        model.addAttribute("nome", usuario.getNome());
+        model.addAttribute("username", usuario.getNomeUsuario());
+        model.addAttribute("usuario", usuario);
         model.addAttribute("avaliacoes", avaliacoesReais);
 
-        model.addAttribute("nomeUsuario", "ANA CAROLINA"); 
-        model.addAttribute("username", "anacarol"); 
 
         return "perfil";
     }
@@ -35,13 +41,13 @@ public class PerfilController {
     public String irParaEdicaoPerfil() {
         return "editaperfil";
     }
-    
+
     @GetMapping("/avaliacao/excluir/{id}")
     public String excluirAvaliacao(@PathVariable String id) {
-        
+
         try {
             avaliacaoService.excluirAvaliacao(id);
-            System.out.println("✅ Excluído com sucesso o ID: " + id);
+            System.out.println(" Excluído com sucesso o ID: " + id);
         } catch (Exception e) {
             System.err.println("Erro ao excluir avaliação: " + e.getMessage());
         }
