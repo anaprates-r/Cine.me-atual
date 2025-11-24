3ba755c4e7b8a8c72493740ca3ba1e66e3e1603f
anaprates-r <ana.prates@ufms.br>
2025-11-24 16:51:42 -0400

    Add existsByNomeUsuario method to UsuarioRepository

--- src/main/java/com/cine/cineme/repository/UsuarioRepository.java
+++ src/main/java/com/cine/cineme/repository/UsuarioRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
     Usuario findByNomeUsuario(String nomeUsuario);
+    boolean existsByNomeUsuario(String nomeUsuario);
 }
